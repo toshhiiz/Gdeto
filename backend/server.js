cf2ac91e82bf8e6db67df53c72f03c14eb9fcfe3
toshhiiz <toshhiiz@github.com>
2026-04-02 16:12:16 +0500

    Update server to use Railway MONGO_URL environment variable

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Подключение к MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL || process.env.MONGO_URI)
   .then(() => console.log('MongoDB подключена'))
   .catch(err => console.error('Ошибка подключения:', err));
 
